94fd6bffdb3bebc7332d0cdda15c67c75164d571
FaridDj <f.djaouti@hotmail.fr>
2024-10-03 21:33:05 +0200

    Mise en place de tous les fichiers du projet

--- motaphoto/functions.php
+++ motaphoto/functions.php
@@ -1,19 +1,16 @@
 <?php
-
 function theme_enqueue_styles() {
     
-    wp_enqueue_style( 'theme-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/css/custom.css' );
     wp_enqueue_script( 'custom-scripts', get_stylesheet_directory_uri() . '/js/scripts.js', array('jquery' ), '1.0', true);
     wp_enqueue_style( 'local-fonts', get_stylesheet_directory_uri() . '/fonts/fonts.css' );
 }
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 
 // Activer les fonctionnalités du thème
-function mytheme_setup() {
-    register_nav_menus(array(
-        'primary' => __('Primary Menu', 'mytheme'),
-    ));
-}
-add_action('after_setup_theme', 'mytheme_setup');
-
-?>
+function register_my_menu(){
+    register_nav_menu('main', "Menu principal");
+    register_nav_menu('footer', "Menu pied de page");
+ }
+ add_action('after_setup_theme', 'register_my_menu');
--- motaphoto/header.php
+++ motaphoto/header.php
@@ -1,12 +1,13 @@
 <!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+<html lang="fr">
 <head>
-    <meta charset="<?php bloginfo('charset'); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF8">
+    <meta name="viewport" content="width=device-width, initial-scale=0">
     <title><?php wp_title('|', true, 'right'); ?></title>
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
+    <!-- Votre en-tête -->
     <header>
         
         <div><img class="logo-img-mini" src="<?php echo get_stylesheet_directory_uri() . '/images/Nathalie Mota2.png'; ?> " alt="Motaphoto"></div>
@@ -15,8 +16,10 @@
         <nav>
     <?php
        get_template_part('templates_part/modale');
-    
+    // Afficher le menu principal avec un bouton pour ouvrir la modale de contact
     wp_nav_menu(array(
+        'theme_location' => 'primary',
+        'menu_class'     => 'primary-menu',
         'items_wrap'     => '<ul id="%1$s" class="%2$s">%3$s <li><a href="#"  id="btn_contact" class="active">Contact</a></li></ul>',
     ));
  
@@ -25,5 +28,3 @@
         </nav>
 
     </header>
-
-    <?php wp_footer();?>
